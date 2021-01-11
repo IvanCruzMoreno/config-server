384f6ada488c47993df0949605bd628c14f1fe67
ivan <ivanmoreno405@gmail.com>
2021-01-11 10:35:59 -0600

    this is a config server with some profiles ans these profiles are saved in git

---
+++ config-server/src/test/java/com/badbadcode/application/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badbadcode.application.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
